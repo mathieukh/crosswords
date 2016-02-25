885361fed8b6fbde0662445306f3815b2b1609a3
Mathieu Khalem <mathieu.khalem@etu.upmc.fr>
2016-02-25 14:14:18 +0100

    - Ajout des grilles prédéfinies dans le pdf

--- main.py
+++ main.py
@@ -9,6 +9,40 @@ import crossword as cr
 
 array = np.array(
 [
+    ['-','-','-','*','*'],
+    ['-','-','-','-','*'],
+    ['-','-','-','-','-'],
+    ['*','-','-','-','-'],
+    ['*','*','-','-','-'],
+])
+
+
+array = np.array(
+[
+    ['*','*','*','-','-','-','*','*','*'],
+    ['*','*','-','-','-','-','-','*','*'],
+    ['*','-','-','-','-','-','-','-','*'],
+    ['-','-','-','-','*','-','-','-','-'],
+    ['-','-','-','*','*','*','-','-','-'],
+    ['-','-','-','-','*','-','-','-','-'],
+    ['*','-','-','-','-','-','-','-','*'],
+    ['*','*','-','-','-','-','-','*','*'],
+    ['*','*','*','-','-','-','*','*','*']
+]);
+
+array = np.array(
+[
+    ['*','-','-','-','*','*','*'],
+    ['-','-','-','-','-','*','*'],
+    ['-','-','*','-','-','-','*'],
+    ['-','-','-','-','-','-','-'],
+    ['*','-','-','-','*','-','-'],
+    ['*','*','-','-','-','-','-'],
+    ['*','*','*','-','-','-','*'],
+])
+"""
+array = np.array(
+[
     ['-','-','-','-','*','-','-','-','-'],
     ['-','*','-','*','*','*','-','*','-'],
     ['-','-','-','-','*','-','-','-','-'],
@@ -19,22 +53,18 @@ array = np.array(
     ['-','*','-','*','*','*','-','*','-'],
     ['-','-','-','-','*','-','-','-','-']
 ]);
-
-dico = [line.rstrip('\n') for line in open('850-mots-us.txt')]
+"""
+dico = [str.upper(line.rstrip('\n')) for line in open('22600-mots-fr.txt')]
+#dico = [str.upper(line.rstrip('\n')) for line in open('58000-mots-us.txt')]
 
 """
 dico = []
-dico += ['SITE','CANS']
+dico += ['SITE','SATE','CANS']
 dico += ['BEES','SIDE']
 dico += ['ANT', 'SUN']
 dico += ['IDAS','AXIS','IDLY','DEER','EAST','READ','SUB','IVE','ENDED','TEE','LOS','SASSY','STAND','ALI','EVE','DRIVE','RED','SEE']
 """
 
 crossword = cr.Crossword(array, dico)
-
-a = cr.Crossword.generateCrossword(8,8,20)
-#crossword.displayCrossword()
-
-#sol = crossword.CSP.solve()
-
-#crosswordSolved = crossword.getCrossword()
+crossword.solve()
+crossword.displayCrossword()
